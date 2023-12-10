594c157d769cb595f940c27636e4e9caf84d8224
muthanii <muthanalsaadi@gmail.com>
2023-12-10 06:54:13 +0300

    Added QR code img file and changed the UI

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
-import vertexai
 from langchain.llms import Cohere
 
 load_dotenv()
 
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
-# PROJECT_ID = os.getenv("PROJECT_ID")
-
-# vertexai.init(project=PROJECT_ID, location="us-central1")
-
 st.sidebar.title("Virtual Agent (Chatbot) using Open Artificial Intelligence")
-st.sidebar.write("This is the graduation project of students from University of Kufa, Department of Electronics and Communications Engineering. It is supervised by Lec. Ammar Mousa. It has been built with a Python backend of LangChain libary with integration from Google Cloud's Vertex AI as the LLM used. The frontend was made possible and hosted by the Python library Streamlit.")
-temp = st.sidebar.slider("Temperature", min_value=0, max_value=1, help="Changes the behaviour of the results. \n\n0 ➡️ **Professional** \n\n 1 ➡️ **Creative**")
-st.sidebar.link_button("Contact", url="https://t.me/muthanii")
-
+st.sidebar.write("This is the graduation project of students from University of Kufa, Department of Electronics and Communications Engineering. It is supervised by Lec. Ammar Mousa. It has been built with a Python backend of LangChain libary with integration from Cohere as the LLM used. The frontend was made possible and hosted by the Python library Streamlit.")
+if st.sidebar.toggle("QR Code"):
+    st.sidebar.image("./img/QR.png")
+st.sidebar.link_button("Contact", url="https://t.me/muthanii", use_container_width=True)
+st.sidebar.link_button("GitHub", url="https://github.com/muthanii/grad-proj", use_container_width=True)
+container = st.container()
+with container:
+    st.title("Virtual Agent (Chatbot)")
 
 def generate_response(input_text):
     llm = Cohere(
